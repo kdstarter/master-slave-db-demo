848ad74f3e7e7c7b90ba7369e534e9397d991d72
Teddy Jiang <teddy.jiang@qq.com>
2022-04-04 19:05:01 +0800

    Added bootstrap and jquery for web frontend pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'home#dashboard'
+  get 'dashboard', to: 'home#dashboard'
 
   namespace :api do
     resources :dashboard, only: [:index]
